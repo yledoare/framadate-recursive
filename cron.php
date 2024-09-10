0f1cbbfcc965186e35be80ea53de007d7c40e41f
Yann Le Doaré <yann@linuxconsole.org>
2024-09-10 21:18:42 +0000

    fix cron.php

--- cron.php
+++ cron.php
@@ -10,10 +10,8 @@ $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
      echo $e->getMessage();
 }
 
-//$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
-
 $sql_recursive_poll="select id from fd_poll where poll_is_recursive = 1";
-$sql_old_slot="select id, title from fd_slot where title < (UNIX_TIMESTAMP() - 86400) and poll_id='";
+$sql_old_slot="select id, title, moments from fd_slot where title < (UNIX_TIMESTAMP() - 86400) and poll_id='";
 
 
 $PollResult = $pdo->query($sql_recursive_poll);
@@ -23,29 +21,45 @@ foreach ($PollResult as $poll) {
 	echo $poll['id'] . PHP_EOL;
 }
 
+$slots=1;
+
 foreach ($polls as $poll) {
 	$newsql=$sql_old_slot. $poll."'";
-	//echo $newsql;
 	$SlotResult = $pdo->query($newsql);
 	foreach ($SlotResult as $slot) {
-		echo $slot['id'] . PHP_EOL;
-		echo $slot['title'] . PHP_EOL;
-		$nextweek=604800 + $slot['title'];
+		echo "SLOT ID: ". $slot['id'] . PHP_EOL;
+		echo "SLOT TITLE:". $slot['title'] . PHP_EOL;
+		echo "Moments : " . $slot['moments'] . PHP_EOL;
+		$moments=explode(",",$slot['moments']);
+		echo "Nb Moments : " . sizeof($moments) . PHP_EOL;
+		$slots=$slots + sizeof($moments);
+
+		// 2 Weeks
+		//$nextweek=1209600  + $slot['title'];
+		
+		// 1 Week
+		$nextweek=604800  + $slot['title'];
+
 		$sql="update fd_slot set title=$nextweek where id=".$slot['id'];
-		//echo $sql . PHP_EOL;
+		echo $sql . PHP_EOL;
 		$stmt= $pdo->prepare($sql);
 		$stmt->execute([$id]);
+		$count = $stmt->rowCount();
+  			echo "$count Slot updated nextweek successfully!". PHP_EOL;
 		$data = [
 		     'title'=> $nextweek,
      		     'id' => $slot['id']
 		];
 
+		/*
 		$sql = "UPDATE fd_slot SET title=:title WHERE id=:id";
+		echo $sql . PHP_EOL;
 		$statement = $pdo->prepare($sql);
 		if($statement->execute($data)) {
 			$count = $statement->rowCount();
-  			echo "$count Slot updated successfully!";
+  			echo "$count Slot updated successfully!" . PHP_EOL;
 		}
+		*/
 		$needshift=TRUE;
 
 	}
@@ -54,7 +68,7 @@ foreach ($polls as $poll) {
 if($needshift)
 {
 	$data = [ 'poll_id' => $poll ];
-	$sql="update fd_vote set choices=substring(choices,2,100) where poll_id=:poll_id";
+	$sql="update fd_vote set choices=substring(choices,$slots,100) where poll_id=:poll_id";
 	$statement = $pdo->prepare($sql);
 	if($statement->execute($data)) {
 		$count = $statement->rowCount();
@@ -65,6 +79,6 @@ if($needshift)
 	$statement = $pdo->prepare($sql);
 	if($statement->execute($data)) {
 		$count = $statement->rowCount();
- 			echo "$count Vote deleted successfully!";
+ 			echo "$count fd_vote deleted successfully!";
 	}
 }
